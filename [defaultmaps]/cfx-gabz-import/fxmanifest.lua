ac852b20b0e783517b3a71d000c7d475612415a0
BrandonSnippets <lockedaxev3@outlook.com>
2022-01-30 14:22:42 -0600

    Map
    Files

---
+++ [defaultmaps]/cfx-gabz-import/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Gabz'
+description 'Import'
+version '4.0.0'
+
+this_is_a_map 'yes'
+
+dependencies {
+    'cfx-gabz-mapdata',
+    '/server:4960',
+    '/gameBuild:2189'
+}
+
+escrow_ignore {
+    'stream/**/*.ytd'
+}
---
+++ [defaultmaps]/cfx-gabz-scenarios/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Gabz'
+description 'Scenarios'
+version '4.0.0'
+
+files {
+    'sp_manifest.ymt',
+}
+
+dependencies {
+    '/server:4960',
+    '/gameBuild:2189'
+}
+
+data_file 'SCENARIO_POINTS_OVERRIDE_PSO_FILE' 'sp_manifest.ymt'
