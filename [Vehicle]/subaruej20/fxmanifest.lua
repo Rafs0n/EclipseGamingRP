f6556555bbbaef91dcdb1c0ba2ce7d139e28faa7
BrandonSnippets <lockedaxev3@outlook.com>
2022-01-30 14:12:58 -0600

    Updated
    Updated

---
+++ [Vehicle]/subaruej20/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/subaruej20_game.dat151.rel',
+  'audioconfig/subaruej20_sounds.dat54.rel',
+  'sfx/dlc_subaruej20/subaruej20.awc',
+  'sfx/dlc_subaruej20/subaruej20_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/subaruej20_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/subaruej20_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_subaruej20'
